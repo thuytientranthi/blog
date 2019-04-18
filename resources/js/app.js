a709b9e9e3c2db5a1996f522440aae18712b8d4f
thuytientranthi <tt.thuytien15@gmail.com>
2019-04-18 09:57:17 +0700

    create view with VueJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,44 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+
+
+import HomeComponent from './components/news/HomeComponent.vue';
+import EditComponent from './components/news/Editcomponent.vue';
+import IndexComponent from './components/news/IndexComponent.vue';
+import NewComponent from './components/news/NewComponent.vue';
+
+const routes = [
+    {
+        name : 'home',
+        path : '/',
+        component : HomeComponent
+    },
+    {
+        name : 'create',
+        path : '/news/create',
+        component : NewComponent
+    },
+    {
+        name : 'new',
+        path : '/news/index',
+        component : IndexComponent
+    },
+    {
+        name : 'edit',
+        path : '/news/edit/:id',
+        component : EditComponent
+    }
+]
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,7 +58,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('home-component', require('./components/news/HomeComponents.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +66,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
---
+++ resources/js/components/news/NewComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+       <h1>Create new</h1>
+        <form action="@submit.prevent='addNew'"></form>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Title:</label>
+                        <!-- post la method -->
+                        <input type="text" class="form-control" v-model="post.title">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Content</label>
+                        <textarea class="form-control" v-model="post.body" rows="5"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Description:</label>
+                        <input type="text" class="form-control" v-model="post.description">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Author</label>
+                        <input type="text" class="form-control" v-model="post.author">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Category</label>
+                        <input type="text" class="form-control" v-model="post.category">
+                    </div>
+                </div>
+            </div><br />
+            <div class="form-group">
+                <button class="btn btn-primary">Create</button>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                post: {}
+            }
+        },
+        methods: {
+            addNews(){
+                Console.log(this.post);
+            }
+        }
+    }
+</script>
